6761798d588357c27ee5087d3cbfeacc65e091ae
Grimshaw Kristóf <h157974@stud.u-szeged.hu>
2025-03-12 13:01:14 +0100

    Eredmények oldalon kiválasztott eredmények megjelennek

--- RunBase_UI_Cypress_Test/cypress/e2e/spec.cy.js
+++ RunBase_UI_Cypress_Test/cypress/e2e/spec.cy.js
@@ -5,7 +5,22 @@ describe('Versenyek megjelennek a versenyek oldalon', () => {
     cy.get('.card').should('exist');
   })
 
+});
+
+describe('Eredmények oldalon versenyek listázhatók', () => {
+  it('passes', () => {
+    cy.visit('https://www.runbase.hu/')
+    cy.get('[href="/eredmenyek"]').click();
+    cy.get('#selectedCompetition').should('not.be.empty');
+  })
+});
+
+describe('Eredmények oldalon kiválasztott eredmények megjelennek lista kiválasztása után', () => {
+  it('passes', () => {
+    cy.visit('https://www.runbase.hu/')
+    cy.get('[href="/eredmenyek"]').click();
+    cy.get('#selectedCompetition').select("27. ALDI Női Futógála");
+    cy.get('.table > :nth-child(2) > :nth-child(1)').should('have.text', '5');
+  })
 })
-
-
 
