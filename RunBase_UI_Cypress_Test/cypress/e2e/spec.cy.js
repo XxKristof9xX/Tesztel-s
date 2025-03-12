30f04b7c3da5d626d98f90c0c9edd8f93318f122
Grimshaw Kristóf <sziaxbox360@gmail.com>
2025-03-12 22:08:58 +0100

    kijelentkezés leellenőrizve

--- RunBase_UI_Cypress_Test/cypress/e2e/spec.cy.js
+++ RunBase_UI_Cypress_Test/cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('Versenyek megjelennek a versenyek oldalon', () => {
   it('passes', () => {
-    cy.visit('https://www.runbase.hu/')
+    cy.visit('/')
     cy.get('[href="/versenyek"]').click();
     cy.get('.card').should('exist');
   })
@@ -9,7 +9,7 @@ describe('Versenyek megjelennek a versenyek oldalon', () => {
 
 describe('Eredmények oldalon versenyek listázhatók', () => {
   it('passes', () => {
-    cy.visit('https://www.runbase.hu/')
+    cy.visit('/')
     cy.get('[href="/eredmenyek"]').click();
     cy.get('#selectedCompetition').should('not.be.empty');
   })
@@ -17,19 +17,32 @@ describe('Eredmények oldalon versenyek listázhatók', () => {
 
 describe('Eredmények oldalon kiválasztott eredmények megjelennek lista kiválasztása után', () => {
   it('passes', () => {
-    cy.visit('https://www.runbase.hu/')
+    cy.visit('/')
     cy.get('[href="/eredmenyek"]').click();
     cy.get('#selectedCompetition').select("27. ALDI Női Futógála");
     cy.get('.table > :nth-child(2) > :nth-child(1)').should('have.text', '5');
   })
 });
 
-describe('Custom command test', () => {
+describe('Bejelentkezésellenőrzése ', () => {
   it('Logs in using cy.loginAs', () => {
-    cy.visit('https://www.runbase.hu/')
-    cy.loginAs('Teszt Felhasználó', 'TitkosJelszo123');
+    const username = 'Teszt Felhasználó';
+    cy.visit('/');
+    cy.loginAs(username, 'TitkosJelszo123');
     cy.get(':nth-child(3) > a.nav-item').contains('Profil').click();
-    cy.get('.card-title').contains('Teszt Felhasználó');
+    cy.get('.card-title').contains(username);
+  });
+});
+
+describe('Kijelentkezés ', () => {
+  it('Logs in using cy.loginAs', () => {
+    const username = 'Teszt Felhasználó';
+    cy.visit('/');
+    cy.loginAs(username, 'TitkosJelszo123');
+    cy.get(':nth-child(3) > a.nav-item').contains('Profil').click();
+    cy.get('.card-title').contains(username);
+    cy.get(':nth-child(3) > .btn').contains('Kijelentkezés').click();
+    cy.get('body').should('not.contain', 'Profil');
   });
 });
 
