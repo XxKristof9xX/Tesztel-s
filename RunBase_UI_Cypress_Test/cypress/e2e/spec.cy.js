9f69b155a4941f8bf586843d2c62cb28aa971f8a
Grimshaw Kristóf <h157974@stud.u-szeged.hu>
2025-03-12 12:18:37 +0100

    versenyek oldal tesztelése

--- RunBase_UI_Cypress_Test/cypress/e2e/spec.cy.js
+++ RunBase_UI_Cypress_Test/cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
+describe('Versenyek megjelennek a versenyek oldalon', () => {
   it('passes', () => {
     cy.visit('https://www.runbase.hu/')
+    cy.get('[href="/versenyek"]').click();
+    cy.get('.card').should('exist');
   })
+
 })
+
+
+
