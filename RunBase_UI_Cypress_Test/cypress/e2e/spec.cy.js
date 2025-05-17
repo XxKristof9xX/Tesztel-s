9504137fd67ef6d9bf8216fbfbf20576a8d92c4d
Grimshaw Kristóf <h157974@stud.u-szeged.hu>
2025-05-17 19:39:23 +0200

    regisztrációs gomb ellenörzése

--- RunBase_UI_Cypress_Test/cypress/e2e/spec.cy.js
+++ RunBase_UI_Cypress_Test/cypress/e2e/spec.cy.js
@@ -128,8 +128,8 @@ describe('Regisztráció rövid jelszóval', () => {
     cy.get('.bg-secondary').contains('Regisztráció').click();
     cy.get('#input-6').type(username);
     cy.get('#input-8').type(password);
-    cy.get('.v-messages__message').contains('A jelszónak legalább 8 karakter hosszúnak kell lennie!');
-    cy.get('.bg-primary').contains('Regisztráció').should('be.disabled');
+    cy.get('.v-messages__message').contains('Minimum 8 karakter hosszú legyen');
+    cy.contains('button', 'Regisztráció').should('be.disabled');
   })
 });
 
@@ -142,8 +142,8 @@ describe('Regisztráció rövid felhasználónévvel', () => {
     cy.get('.bg-secondary').contains('Regisztráció').click();
     cy.get('#input-6').type(username);
     cy.get('#input-8').type(password);
-    cy.get('.v-messages__message').contains('A felhasználónévnek legalább 6 karakter hosszúnak kell lennie!');
-    cy.get('.bg-primary').contains('Regisztráció').should('be.disabled');
+    cy.get('.v-messages__message').contains('Minimum 6 karakter hosszú legyen');
+    cy.contains('button', 'Regisztráció').should('be.disabled');
   })
 });
 
